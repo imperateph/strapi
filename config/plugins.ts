94796725a649f6e67529dbe5fef51ddaee494942
John Roice Aldeza <jroicealdeza@gmail.com>
2024-10-11 23:57:15 +0800

    Update AWS S3 configuration in plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,8 +3,6 @@ export default ({env}) => ({
         config: {
           provider: 'aws-s3',
           providerOptions: {
-            // baseUrl: env('CDN_URL'),
-            // rootPath: env('CDN_ROOT_PATH'),
             s3Options: {
               credentials: {
                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
@@ -12,8 +10,6 @@ export default ({env}) => ({
               },
               region: env('AWS_REGION'),
               params: {
-                // ACL: env('AWS_ACL', 'public-read'),
-                // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
                 Bucket: env('AWS_BUCKET'),
               },
             },
